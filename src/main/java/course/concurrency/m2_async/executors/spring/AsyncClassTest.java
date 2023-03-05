c42a664ad4fad6dccafcc496f92c706b1b2b1e94
Shiki <indigoleague23@gmail.com>
2023-03-05 21:43:48 +0300

    run internalTask asynchronously getting bean by field injection

--- src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
@@ -3,6 +3,7 @@ package course.concurrency.m2_async.executors.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
@@ -14,13 +15,17 @@ public class AsyncClassTest {
     public ApplicationContext context;
 
     @Autowired
+    @Lazy
+    public AsyncClassTest asyncClassTest;
+
+    @Autowired
     @Qualifier("applicationTaskExecutor")
     private ThreadPoolTaskExecutor executor;
 
     @Async
     public void runAsyncTask() {
         System.out.println("runAsyncTask: " + Thread.currentThread().getName());
-        context.getBean(AsyncClassTest.class).internalTask();
+        asyncClassTest.internalTask();
     }
 
     @Async
