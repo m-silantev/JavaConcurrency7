c5655dfd94523ab5f15560ae01fb66eef61b3756
Shiki <indigoleague23@gmail.com>
2023-03-09 00:02:42 +0300

    [2-3-3] use cached thread pool (cuz of unlimited number of threads)

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -3,11 +3,14 @@ package course.concurrency.m2_async.cf.min_price;
 import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class PriceAggregator {
 
     private final double naN = Double.NaN;
+    private final ExecutorService executor = Executors.newCachedThreadPool();
     private PriceRetriever priceRetriever = new PriceRetriever();
     private Collection<Long> shopIds = Set.of(10L, 45L, 66L, 345L, 234L, 333L, 67L, 123L, 768L);
 
@@ -22,8 +25,8 @@ public class PriceAggregator {
 
     public double getMinPrice(long itemId) {
         var completableFutures = shopIds.stream()
-                .map(shopId -> CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(itemId, shopId))
-                        .completeOnTimeout(naN, 2, TimeUnit.SECONDS)
+                .map(shopId -> CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(itemId, shopId), executor)
+                        .completeOnTimeout(naN, 2900, TimeUnit.MILLISECONDS)
                         .handle((price, ex) -> ex == null ? price : naN))
                 .toList();
         return completableFutures.stream()
