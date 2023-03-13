677b93cd1b7f18f9d61576e91fcc73a9d1062087
Shiki <indigoleague23@gmail.com>
2023-03-13 12:51:50 +0300

    [3-1] add dependencies

--- src/main/java/course/concurrency/m3_shared/benchmarks/CounterBenchmarks.java
+++ src/main/java/course/concurrency/m3_shared/benchmarks/CounterBenchmarks.java
@@ -1,6 +1,13 @@
 package course.concurrency.m3_shared.benchmarks;
 
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Group;
+import org.openjdk.jmh.annotations.GroupThreads;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -10,7 +17,11 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.LongAdder;
-import java.util.concurrent.locks.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.StampedLock;
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.Throughput)
